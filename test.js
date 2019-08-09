8a02f8106f7cfe8a1daba1099ba44d8d85cebabc
yazimei <41285833+yazimei@users.noreply.github.com>
2019-08-09 14:52:13 +0800

    Update test.js

--- test.js
+++ test.js
@@ -52,3 +52,24 @@
 								this.uploadFile()
 							})
 						}        
+
+
+// base64转blob
+			dataBlob(dataurl) {
+				var arr = dataurl.split(','),
+					mime = arr[0].match(/:(.*?);/)[1],
+					bstr = atob(arr[1]),
+					n = bstr.length,
+					u8arr = new Uint8Array(n);
+				while (n--) {
+					u8arr[n] = bstr.charCodeAt(n);
+				}
+				return new Blob([u8arr], {
+					type: mime
+				})
+			},
+			// blob转文件
+			blobFile(theBlob) {
+				theBlob.lastModifiedDate = new Date();
+				return theBlob;
+			},
